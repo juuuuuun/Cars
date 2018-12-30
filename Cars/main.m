71f85f1b5f0c1a49a0e2ec7f7247b7d83d467a41
Jun Oh <kyungjunjoh@gmail.com>
2018-12-29 17:40:57 -0800

    Initial Commit

---
+++ Cars/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cars
+//
+//  Created by Jun Oh on 2018-12-29.
+//  Copyright © 2018 Jun Oh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
